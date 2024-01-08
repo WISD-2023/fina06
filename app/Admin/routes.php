0e0093f26192effc30cf9558afe1ad161554fa06
3B032063 <3B032063@gm.student.ncut.edu.tw>
2024-01-09 02:49:43 +0800

    建立買家管理後台

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use Encore\Admin\Controllers\UserController;
 use Illuminate\Routing\Router;
 
 Admin::routes();
@@ -12,5 +13,5 @@ Route::group([
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index')->name('home');
-
+    $router->resource('users', UserController::class);
 });
