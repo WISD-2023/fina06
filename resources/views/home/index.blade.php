edb1291f0155fd36e5246d2998c0939d38382982
3B032063 <3B032063@gm.student.ncut.edu.tw>
2024-01-08 23:32:30 +0800

    設定首頁圖片

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('首頁') }}
+            <img src="img/1.jpg" alt="Descriptive Text">
         </h2>
     </x-slot>
 
